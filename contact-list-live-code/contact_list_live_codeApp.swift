1a99e7ac3b6ea455fe9537096115c153ebfb2fd1
Trien Vo <s3907397@rmit.edu.vn>
2023-08-04 15:56:14 +0700

    Initial Commit

---
+++ contact-list-live-code/contact_list_live_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  contact_list_live_codeApp.swift
+//  contact-list-live-code
+//
+//  Created by Trien Vo Hong on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct contact_list_live_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
